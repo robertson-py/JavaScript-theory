e7e4420b08b79764d15b5ffb19ba30ed989f8a00
Roberto Jordán López <68826095+robertson-py@users.noreply.github.com>
2022-05-31 21:15:19 -0300

    Add files via upload

---
+++ theory.js
@@ -0,0 +1,22 @@
+// There is a convention that all the variables in JavaScript are written in "camel case". Here is an example:
+const firstNamePerson = "John"; // camel case style
+const first_name_person = "John"; // this style is more used in Python
+// Variables can't start with a number.
+// They only admit letters, numbers and the dollar sign.
+// Variables that won't change, we usually write them in caps lock and with the reserved word "const".
+
+// Other form to declare a varible it's with the reserved word "let".
+let lastName = "Smith";
+// Normally, the "let" variables will change in the future. "const" are with a fixed value.
+
+/*---snip---*/
+
+// The seven primitive data types are:
+let age = 27;           // 1) Number (integer and floats)
+const name = "John";    // 2) Strings
+let running = false;    // 3) Boolean (true or false)
+let nope;               // 4) Undefined (declare a variable without a value)
+// The next ones are rarely used. It's only for very especific tasks.
+// 5) Null (also means empty value)
+// 6) Symbol
+// 7) BigInt
